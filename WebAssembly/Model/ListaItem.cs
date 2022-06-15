354f1a6dffd9867ce289b80d7645225c35caecef
wfacciolla2 <facciollacorp@gmail.com>
2022-06-15 13:54:49 -0300

    Adicionar arquivos de projeto.

---
+++ WebAssembly/Model/ListaItem.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAssembly.Model
+{
+    public class ListaItem
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
